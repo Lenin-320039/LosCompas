a65f05b75057d0aa45406f783208ea1d59198cb9
johanfsl <tic-320080@utnay.edu.mx>
2025-10-08 19:03:12 -0700

    Update lab4.py
    epico

--- lab4.py
+++ lab4.py
@@ -4,8 +4,9 @@ import random
 
 #* Semilla para reproducibilidad
 rng = np.random.default_rng(42)  
- 
+
 #* 3 entradas → 4 salidas
+
 W = rng.random((3, 4))          # Matriz de pesos 3x4
 b = rng.random(4)               # Vector de sesgo
 x = np.array([1.0, 0.5, -1.0])  # Vector de entrada (3 valores)
@@ -15,6 +16,10 @@ x = np.array([1.0, 0.5, -1.0])  # Vector de entrada (3 valores)
 y = x @ W  # Equivalente a np.dot(x, W)
 #*np.dot()
 
+def sigmoid(z):
+  return 1 / (1 + np.exp(-z))
+factivado = sigmoid(y)
+
 #* Mostrar resultados
 print("Matriz de pesos (3x4):")
 print(W)
@@ -22,6 +27,9 @@ print("\nEntrada:")
 print(x)
 print("\nSalida:")
 print(y)
+print("\nSalida Despues de la activacion Sigmoid:")
+print(factivado)
+
 
 #trabajo en equipo
-
+#epico
